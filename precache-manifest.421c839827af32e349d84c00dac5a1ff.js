95cf01a985fb3362e0282af476e777226732985b
Damià V.F <damia.vilardell@smartive.eu>
2020-06-27 19:39:09 +0200

    Updates

---
+++ precache-manifest.421c839827af32e349d84c00dac5a1ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb66cfdd54c64540258ace0a7dbc7e8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2bcfe77d8eb01abcf6de",
+    "url": "/monsters-rolodex/static/css/main.a356fa4b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bcfe77d8eb01abcf6de",
+    "url": "/monsters-rolodex/static/js/main.a99a362d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
